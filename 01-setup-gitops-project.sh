ad38d45290ba93e51fb97e314f1814faf75199e8
mmartofel <mmartofe@redhat.com>
2022-11-27 22:36:02 +0100

    istio gateway name fix

--- 01-setup-gitops-project.sh
+++ 01-setup-gitops-project.sh
@@ -1,3 +1,3 @@
 oc apply -f ./argo/appProject.yaml -n dev
 oc apply -f ./argo/application-bookinfo.yaml -n dev
-oc apply -f ./argo/application-bookinfo-servicemash-networking.yaml -n dev
+oc apply -f ./argo/application-bookinfo-servicemesh-networking.yaml -n dev
